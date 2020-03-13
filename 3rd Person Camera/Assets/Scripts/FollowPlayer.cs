53f3b33e313b2a03082f9cde5873e608d0ef5093
ManChunFu <man.chunfu@futuregames.nu>
2020-03-13 21:04:13 +0100

    refactory

--- 3rd Person Camera/Assets/Scripts/FollowPlayer.cs
+++ 3rd Person Camera/Assets/Scripts/FollowPlayer.cs
@@ -6,28 +6,34 @@ public class FollowPlayer : MonoBehaviour
 {
     [Header("Camera follow target adjustment")]
     [SerializeField] private Transform _target = default;
-    [SerializeField] private float _distanceToTarget = 5f;
-    [SerializeField, Range(0.5f, 25.0f)] private float _smoothSpeed = 6.0f;
+    [Tooltip("Camera's height : affect its position.y value")]
+    [SerializeField, Range(0.0f, 10.0f)] private float _attitude = 0.7f;
+    [Tooltip("Affecting camera's rotaiton.x value")]
+    [SerializeField, Range(0.0f, 20.0f)] private float _viewAngle = 8.0f;
+    [SerializeField] private float _distanceToTarget = 4.2f;
+    [SerializeField, Range(0.5f, 25.0f)] private float _smoothSpeed = 3.5f;
 
     [Header("Mouse rotate camera view")]
     [SerializeField, Range(10.0f, 70.0f)] private float _mouseSensitivity = 45.0f;
+    [Tooltip("Prevent vertical rotation go around the target, max value can not be smaller than min value")]
     [SerializeField, Range(-89f, 89f)] private float _minVerticalAngle = -10.0f, _maxVerticalAngle = 40.0f;
 
     [Header("Camera zoom")]
-    [SerializeField] private float _zoomSpeed = 80.0f;
+    [SerializeField] private float _zoomSpeed = 90.0f;
     [SerializeField] private float _maxZoom = 15.0f;
     [SerializeField] private float _minZoom = 2.0f;
 
     [Header("Camera view gets blocked")]
+    [Tooltip("Setup the layer that camera should react with")]
     [SerializeField] private LayerMask _collisionMask = default;
-    [SerializeField, Min(0f)] float _minDistance = 1.8f;
-    
+    [Tooltip("The distance betwen the target and the obstacle")]
+    [SerializeField, Range(1.0f, 2.0f)] float _zoomIn = 1.8f;
+    [SerializeField, Range(1.0f, 10.0f)] private float _smoothZoomToTarget = 6.0f;
+
     private Vector3 _cameraRotation = Vector3.zero;
     private Vector3 _lookDirection = Vector3.zero;
     private float _zoomInput = 0.0f;
-    private Ray _ray;
-    private RaycastHit _raycastHit;
-    private float _adjustedDistance;
+    private float _adjustedDistance = 0;
     private bool _isCameraAdjusted = false;
 
     #region Input IDs
@@ -46,8 +52,8 @@ public class FollowPlayer : MonoBehaviour
     private void Awake()
     {
         Assert.IsNotNull(_target, "Did you forget to assign the target?");
-
         _cameraRotation = transform.localRotation.eulerAngles;
+        _cameraRotation.x = _viewAngle;
     }
 
 
@@ -58,24 +64,25 @@ public class FollowPlayer : MonoBehaviour
 
     private void LateUpdate()
     {
-       MouseRotation();
-       _lookDirection = Quaternion.Euler(_cameraRotation) * Vector3.forward;
-       CameraCollision();
-       Vector3 lookPosition = _target.position - _lookDirection * _adjustedDistance;
-       transform.position = Vector3.Slerp(transform.position, lookPosition, _smoothSpeed * Time.unscaledDeltaTime);
+        MouseRotation();
+        _lookDirection = Quaternion.Euler(_cameraRotation) * Vector3.forward;
+        CameraCollision();
+        Vector3 lookPosition = _target.position + (_isCameraAdjusted ? Vector3.zero : new Vector3(0.0f, _attitude, 0.0f)) - _lookDirection * _adjustedDistance;
+        transform.position = Vector3.Slerp(transform.position, lookPosition, (_isCameraAdjusted ? _smoothZoomToTarget : _smoothSpeed) * Time.unscaledDeltaTime);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(_target.position, _minDistance);
+        Gizmos.DrawWireSphere(_target.position, _zoomIn);
     }
 
     private void MouseRotation()
     {
         _cameraRotation.y += Input.GetAxis(_rotateHorizontally) * _mouseSensitivity * Time.deltaTime;
-        _cameraRotation.x -= Input.GetAxis(_rotateVertically) * _mouseSensitivity * Time.deltaTime;
+        _cameraRotation.x += -Input.GetAxis(_rotateVertically) * _mouseSensitivity * Time.deltaTime;
         _cameraRotation.x = Mathf.Clamp(_cameraRotation.x, _minVerticalAngle, _maxVerticalAngle);
+
         transform.eulerAngles = _cameraRotation;
     }
 
@@ -88,21 +95,17 @@ public class FollowPlayer : MonoBehaviour
 
     private void CameraCollision()
     {
-        float camDistance = _distanceToTarget;
-        _ray.origin = _target.position;
-        _ray.direction = - _lookDirection;
+        Ray ray = new Ray();
+        ray.origin = _target.position;
+        ray.direction = -_lookDirection;
+        Debug.DrawLine(ray.origin, ray.direction, Color.red);
 
-        Debug.DrawLine(_ray.origin, _ray.direction, Color.red);
-
-        if (Physics.Raycast(_ray, out _raycastHit, camDistance, _collisionMask))
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, _distanceToTarget, _collisionMask))
         {
             if (!_isCameraAdjusted)
             {
-                if (Vector3.Distance(_ray.origin, _raycastHit.point) < _minDistance)
-                {
-                    _adjustedDistance = _minDistance;
-                    _isCameraAdjusted = true;
-                }
+                _adjustedDistance = _zoomIn;
+                _isCameraAdjusted = true;
             }
         }
         else
